e0ff18bf72d9e41429c787dcbe99049fa997a3b2
dona8833 <temitopeabdulsamad2@gmail.com>
2025-09-27 07:41:24 +0100

    finally resolved path error

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Nav from '../components/collectionscomponent/Nav'
 import Layout from '../components/Layout'
-import Collect from '../components/collectionscomponent/collect'
+import Collect from '../components/collectionscomponent/Colate.jsx'
 
 function Collection() {
   return (
